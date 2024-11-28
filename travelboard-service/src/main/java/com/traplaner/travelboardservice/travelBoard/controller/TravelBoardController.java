a4f35649d14fe149d4a72b210437e2837f7070de
sup226 <rlaalswl1102@naver.com>
2024-11-28 17:04:43 +0900

    TB Controller, Service 해야 함!!

--- travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/TravelBoardController.java
+++ travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/TravelBoardController.java
@@ -1,14 +1,35 @@
 package com.traplaner.travelboardservice.travelBoard.controller;
 
+import com.traplaner.travelboardservice.travelBoard.dto.TravelBoardDetailResponseDTO;
+import com.traplaner.travelboardservice.travelBoard.dto.TravelBoardListResponseDTO;
+import com.traplaner.travelboardservice.travelBoard.service.TravelBoardService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/travelboard")
 @RequiredArgsConstructor
 @Slf4j
 public class TravelBoardController {
+    private final TravelBoardService travelBoardService;
 
+    @GetMapping("/list")
+    public ResponseEntity<TravelBoardListResponseDTO> getTravelBoardList() {
+        return ResponseEntity.ok(travelBoardService.getTravelBoardList());
+    }
+
+    @GetMapping("/info/{boardId}")
+    public ResponseEntity<TravelBoardDetailResponseDTO> getTravelBoardDetail(
+            @PathVariable Integer boardId) {
+        return ResponseEntity.ok(travelBoardService.getTravelBoardDetail(boardId));
+    }
 }
--- travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/service/TravelBoardService.java
+++ travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/service/TravelBoardService.java
@@ -1,14 +1,29 @@
 package com.traplaner.travelboardservice.travelBoard.service;
 
+import com.traplaner.travelboardservice.travelBoard.dto.TravelBoardDetailResponseDTO;
+import com.traplaner.travelboardservice.travelBoard.dto.TravelBoardListResponseDTO;
+import com.traplaner.travelboardservice.travelBoard.repository.FavoriteRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @Getter
 public class TravelBoardService {
+    private final FavoriteRepository favoriteRepository;
 
+    public TravelBoardListResponseDTO getTravelBoardList() {
+        return null;
+    }
+
+    public TravelBoardDetailResponseDTO getTravelBoardDetail(Integer boardId) {
+        return null;
+    }
 }
