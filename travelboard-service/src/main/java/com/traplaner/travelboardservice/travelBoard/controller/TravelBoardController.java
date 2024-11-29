fd2de64b4cff3c58ed321a6ff0df4e8da780d8d7
sup226 <rlaalswl1102@naver.com>
2024-11-29 16:25:46 +0900

    tb controller, service

--- travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/TravelBoardController.java
+++ travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/TravelBoardController.java
@@ -10,12 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/travelboard")
 @RequiredArgsConstructor
 @Slf4j
--- travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/service/TravelBoardService.java
+++ travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/service/TravelBoardService.java
@@ -1,23 +1,28 @@
 package com.traplaner.travelboardservice.travelBoard.service;
 
+import com.traplaner.travelboardservice.client.MemberServiceClient;
+import com.traplaner.travelboardservice.client.MypageServiceClient;
+import com.traplaner.travelboardservice.client.TravelplanServiceClient;
 import com.traplaner.travelboardservice.travelBoard.dto.TravelBoardDetailResponseDTO;
 import com.traplaner.travelboardservice.travelBoard.dto.TravelBoardListResponseDTO;
 import com.traplaner.travelboardservice.travelBoard.repository.FavoriteRepository;
+import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @Getter
+@Transactional
 public class TravelBoardService {
     private final FavoriteRepository favoriteRepository;
+
+    private final MemberServiceClient memberServiceClient;
+    private final TravelplanServiceClient travelplanServiceClient;
+    private final MypageServiceClient mypageServiceClient;
 
     public TravelBoardListResponseDTO getTravelBoardList() {
         return null;
