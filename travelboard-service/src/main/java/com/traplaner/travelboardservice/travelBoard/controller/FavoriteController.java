12d6c4200c6b062f8519990a56958bfb4cf9e428
sup226 <rlaalswl1102@naver.com>
2024-12-02 15:05:13 +0900

    top3 FavoriteController 수정 (잘됨)

--- travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/FavoriteController.java
+++ travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/FavoriteController.java
@@ -1,9 +1,11 @@
 package com.traplaner.travelboardservice.travelBoard.controller;
 
+import com.traplaner.travelboardservice.common.dto.CommonResDto;
 import com.traplaner.travelboardservice.travelBoard.dto.response.FavoriteResDTO;
 import com.traplaner.travelboardservice.travelBoard.service.FavoriteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +13,16 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/favorites")
 @Slf4j
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
 
-    @GetMapping("/topThree")
+    @GetMapping("/top3-favorite")
     public ResponseEntity<List<FavoriteResDTO>> getTopThreeFavorites() {
         List<FavoriteResDTO> topThreeFavorites = favoriteService.getTopThree();
-        return ResponseEntity.ok(topThreeFavorites);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "top3 좋아요 조회 완료!", topThreeFavorites);
+        return new ResponseEntity(commonResDto, HttpStatus.OK);
     }
 
 }
