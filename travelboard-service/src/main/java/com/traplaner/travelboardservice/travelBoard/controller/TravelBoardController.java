f17186fe8da2b98b49ae388bbf7477c705a0cfc0
sup226 <rlaalswl1102@naver.com>
2024-12-02 15:05:51 +0900

    return 형식 바꿈

--- travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/TravelBoardController.java
+++ travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/TravelBoardController.java
@@ -1,5 +1,6 @@
 package com.traplaner.travelboardservice.travelBoard.controller;
 
+import com.traplaner.travelboardservice.common.dto.CommonResDto;
 import com.traplaner.travelboardservice.travelBoard.dto.TravelBoardInfoDTO;
 import com.traplaner.travelboardservice.travelBoard.dto.TravelBoardListDTO;
 import com.traplaner.travelboardservice.travelBoard.dto.response.MemberDTO;
@@ -10,11 +11,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/travelboard")
 @RequiredArgsConstructor
 @Slf4j
 public class TravelBoardController {
@@ -23,12 +24,16 @@ public class TravelBoardController {
 
     @GetMapping("/list")
     public ResponseEntity<Page<TravelBoardListDTO>> getTravelBoardList(Pageable pageable) {
-        return ResponseEntity.ok(travelBoardService.getTravelBoardList(pageable));
+        Page<TravelBoardListDTO> list = travelBoardService.getTravelBoardList(pageable);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "보드 리스트 조회 완료!", list);
+        return new ResponseEntity(commonResDto, HttpStatus.OK);
     }
 
     @GetMapping("/info/{boardId}")
     public ResponseEntity<TravelBoardInfoDTO> getTravelBoardInfo(@PathVariable("boardId") Integer boardId) {
-        return ResponseEntity.ok(travelBoardService.getTravelBoardInfo(boardId));
+        TravelBoardInfoDTO info = travelBoardService.getTravelBoardInfo(boardId);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "보드 상세 조회 완료!", info);
+        return new ResponseEntity(commonResDto, HttpStatus.OK);
     }
 
     @PostMapping("/{boardId}/toggle-like")
