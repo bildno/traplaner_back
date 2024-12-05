50790fc2b0987a9804cdbffa2f97e398ce64e3a7
sup226 <rlaalswl1102@naver.com>
2024-12-05 16:04:40 +0900

    이동

--- travelboard-service/src/main/java/com/traplaner/travelboardservice/client/TravelplanServiceClient.java
+++ travelboard-service/src/main/java/com/traplaner/travelboardservice/client/TravelplanServiceClient.java
@@ -1,8 +1,8 @@
 package com.traplaner.travelboardservice.client;
 
 import com.traplaner.travelboardservice.common.dto.CommonResDto;
-import com.traplaner.travelboardservice.travelBoard.dto.response.JourneyDTO;
-import com.traplaner.travelboardservice.travelBoard.dto.response.TravelDTO;
+import com.traplaner.travelboardservice.travelBoard.dto.request.JourneyDTO;
+import com.traplaner.travelboardservice.travelBoard.dto.request.TravelDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
