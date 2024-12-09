6411ffaec4df48f8f0c4198056d04868e975e5ef
sup226 <rlaalswl1102@naver.com>
2024-12-09 10:51:42 +0900

    현재 좋아요 상태 확인

--- travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/FavoriteController.java
+++ travelboard-service/src/main/java/com/traplaner/travelboardservice/travelBoard/controller/FavoriteController.java
@@ -5,6 +5,7 @@ import com.traplaner.travelboardservice.common.dto.CommonResDto;
 import com.traplaner.travelboardservice.travelBoard.dto.FavoriteDTO;
 import com.traplaner.travelboardservice.travelBoard.dto.response.FavoriteResDTO;
 import com.traplaner.travelboardservice.travelBoard.entity.Favorite;
+import com.traplaner.travelboardservice.travelBoard.repository.FavoriteRepository;
 import com.traplaner.travelboardservice.travelBoard.service.FavoriteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,16 +40,28 @@ public class FavoriteController {
                                         @PathVariable Integer boardId) {
 
         int memberId = Integer.parseInt(userInfo.getId());
-        Long likeCount = favoriteService.toggleLike(boardId,memberId);
+        Long likeCount = favoriteService.toggleLike(boardId, memberId);
 
         CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "좋아요를 눌렀습니다.", likeCount);
 
         return new ResponseEntity(commonResDto, HttpStatus.OK);
     }
 
+    @GetMapping("/toggle-like/status/{boardId}")
+    @ResponseBody
+    public ResponseEntity<?> checkLikeStatus(@AuthenticationPrincipal TokenUserInfo userInfo,
+                                             @PathVariable Integer boardId) {
+        int memberId = Integer.parseInt(userInfo.getId());
+        boolean isLiked = favoriteService.isLiked(boardId, memberId);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "좋아요를 누른 상태입니다.", isLiked);
+
+        return new ResponseEntity(commonResDto, HttpStatus.OK);
+    }
+
     @GetMapping("/my-favoriteList/{id}")
-    public ResponseEntity<?> getMyFavorites(@PathVariable int id, Pageable pageable) {
-        Page<Favorite> myFavorites = favoriteService.myFavorites(id, pageable);
+    public ResponseEntity<?> getMyFavorites(@PathVariable("id") TokenUserInfo userInfo, Pageable pageable) {
+        int memberId = Integer.parseInt(userInfo.getId());
+        Page<Favorite> myFavorites = favoriteService.myFavorites(memberId, pageable);
         CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "내가 좋아요한 게시물 조회 완료!", myFavorites);
         return new ResponseEntity(commonResDto, HttpStatus.OK);
     }
